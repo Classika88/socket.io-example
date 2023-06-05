47d01fb561771e0b60f8c30ea3f8b4694452af19
Iryna Khamidova <waterholein@gmail.com>
2023-06-05 09:22:15 +0300

    Add group chat

--- server/app.js
+++ server/app.js
@@ -10,7 +10,9 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket)=> {
-    console.log("New frontend connect")
+   socket.on("chat-message", message => {
+    socket.broadcast.emit("chat-message", message);
+   })
 });
 
 httpServer.listen(3001);
